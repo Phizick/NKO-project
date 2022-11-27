7215d1588811841b3b39dc1322380ad5d2762078
Doretskov Igor <49729545+ixxxar@users.noreply.github.com>
2022-11-27 19:23:52 +0500

    Header, Burger Menu, Questions
    Компонент Questions закомментирован в MainPage

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
+import Questions from "../../components/Questions/Questions";
 import styleMainPage from "./MainPage.module.css";
 
 export const MainPage = () => {
   return (
     <div>
       <Header />
+      {/* <Questions /> */}
       <h1>MainPage</h1>
     </div>
   );
