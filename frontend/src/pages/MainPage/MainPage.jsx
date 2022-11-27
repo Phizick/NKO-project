81a263943f0a371d1b85c4007575e95a24e556a9
UliaKarpova <chelovek-e-95@mail.ru>
2022-11-27 19:52:53 +0300

    feat news

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import styleMainPage from "./MainPage.module.css";
+import News from "../../components/News/News";
 
 export const MainPage = () => {
   return (
     <div>
       <h1>MainPage</h1>
+      <News />
     </div>
   );
 };
