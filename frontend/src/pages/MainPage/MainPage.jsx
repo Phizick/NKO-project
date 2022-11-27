bc456c1aa1b629e972a2a2e3bebdcb721fa9c83f
UliaKarpova <chelovek-e-95@mail.ru>
2022-11-27 22:36:00 +0300

    feat/partners

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import styleMainPage from "./MainPage.module.css";
+import Partners from "../../components/Partners/Partners";
 
 export const MainPage = () => {
   return (
     <div>
       <h1>MainPage</h1>
+      <Partners />
     </div>
   );
 };
