03301d1047197af2285cc6d4214919c3e43634ec
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-11-27 18:01:00 +0300

    feat footer

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import styleMainPage from "./MainPage.module.css";
+import Footer from "../../components/Footer/Footer";
 
 export const MainPage = () => {
   return (
     <div>
       <h1>MainPage</h1>
+      <Footer/>
     </div>
   );
 };
