bd2e3a142fb9a186b2c0168019dcba91c6b7538a
Doretskov Igor <49729545+ixxxar@users.noreply.github.com>
2022-11-27 16:43:11 +0500

    Header, Burger Menu, Questions

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,10 @@
+import Header from "../../components/Header/Header";
 import styleMainPage from "./MainPage.module.css";
 
 export const MainPage = () => {
   return (
     <div>
+      <Header />
       <h1>MainPage</h1>
     </div>
   );
