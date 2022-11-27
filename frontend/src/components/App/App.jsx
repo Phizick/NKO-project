52bae6bb4062c33508c00cdfdcd5d588fb81453b
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2022-11-27 20:40:47 +0300

    feat: add programs component

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "react-router-dom";
 import { MainPage } from "../../pages/MainPage/MainPage";
+import Programs from "../Programs/Programs";
 import styleApp from "./App.module.css";
 
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
       <Switch>
         <Route path="/" exact={true}>
           <MainPage />
+          <Programs />
         </Route>
         <Route path="*">
           <h1>Error Page</h1>
