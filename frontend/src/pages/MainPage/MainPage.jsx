909b241abf7eafbdcb0c7af7b8ac19b6a699e2d8
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2022-11-28 19:31:39 +0300

    feat: add donation on main page

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,20 @@
-import styleMainPage from "./MainPage.module.css";
+import DonateForm from "../../components/Donate/DonateForm";
+import DonateReason from "../../components/Donate/DonateReason";
+import "./MainPage.module.css";
 
 export const MainPage = () => {
   return (
-    <div>
-      <h1>MainPage</h1>
-    </div>
+    <>
+      <div>
+        <h1>MainPage</h1>
+      </div>
+      <section className="donate">
+        <DonateReason />
+        <h2 className="donate__title donate__title_type_root">Меняй мир к лучшему с нами!</h2>
+        <p className="donate__paragraph">более 1 000 человек</p>
+        <p className="donate__paragraph">уже получили помощь</p>
+        <DonateForm />
+      </section>
+    </>
   );
 };
