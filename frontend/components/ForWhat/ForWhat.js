d1d6c80e9d31336eff3a264470019d45bc75e781
Nick <nick@Nicks-MacBook.local>
2022-12-02 17:21:37 +0300

    feat:(Document): исправил Image

--- frontend/components/ForWhat/ForWhat.js
+++ frontend/components/ForWhat/ForWhat.js
@@ -3,6 +3,7 @@ import styles from './ForWhat.module.css';
 import personal from '../../images/personal.png';
 import brushes from '../../images/brushes.png';
 import box from '../../images/box.png';
+import Image from 'next/image'
 
 function ForWhat() {
     return (
@@ -10,21 +11,21 @@ function ForWhat() {
             <h2 className={ styles.title }>На что нужны пожертвования</h2>
             <ul className={ styles.list }>
                 <li className={ styles.item }>
-                    <img src={personal} className={styles.image} alt='Персонал' />
+                    <Image src={personal} className={styles.image} alt='Персонал' />
                     <div className={styles.itemTextBlock}>
                         <h3 className={styles.itemTitle}>Зарплата персонала</h3>
                         <p className={styles.itemText}>Мы стараемся приглашать в фонд квалифицированных специалистов</p>
                     </div>
                 </li>
                 <li className={ styles.item }>
-                    <img src={brushes} className={styles.image} alt='Инвентарь' />
+                    <Image src={brushes} className={styles.image} alt='Инвентарь' />
                     <div className={styles.itemTextBlock}>
                         <h3 className={styles.itemTitle}>Инвентарь</h3>
                         <p className={styles.itemText}>Для проведения различных активностей требуется инвентарь.</p>
                     </div>
                 </li>
                 <li className={ styles.item }>
-                    <img src={box} className={styles.image} alt='Логистика' />
+                    <Image src={box} className={styles.image} alt='Логистика' />
                     <div className={styles.itemTextBlock}>
                         <h3 className={styles.itemTitle}>Логистика</h3>
                         <p className={styles.itemText}>Lorem ipsum dolor sit amet consectetur.</p>
